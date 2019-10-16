c57ac60c845c20312e8d5086d7171f2d8ac9fadb
Thanatat Tamtan <acoshift@gmail.com>
2019-10-16 09:54:52 +0700

    stream file directly

--- backup.sh
+++ backup.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 export PATH=$PATH:/root/google-cloud-sdk/bin
 
-pg_dumpall > dump || exit 1
-gsutil cp -Z dump gs://$BUCKET/$PERFIX$(date +"%Y%m%d%H%M%S")$SUFFIX
+pg_dumpall | gsutil cp -Z - gs://$BUCKET/$PERFIX$(date +"%Y%m%d%H%M%S")$SUFFIX
