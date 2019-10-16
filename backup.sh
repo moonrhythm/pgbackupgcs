1d74ab8fecbf0cbc9d9aece2964d6a93510236c0
Thanatat Tamtan <acoshift@gmail.com>
2019-10-16 10:01:47 +0700

    use gzip command instead of -Z

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export PATH=$PATH:/root/google-cloud-sdk/bin
 
-pg_dumpall | gsutil cp -Z - gs://$BUCKET/$PERFIX$(date +"%Y%m%d%H%M%S")$SUFFIX
+pg_dumpall | gzip | gsutil cp - gs://$BUCKET/$PERFIX$(date +"%Y%m%d%H%M%S")$SUFFIX.sql.gz
