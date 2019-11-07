3181b2c06a7f18a04ed9e28e215edcfe952a75a2
Thanatat Tamtan <acoshift@gmail.com>
2019-11-07 08:03:17 +0700

    fix typo

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export PATH=$PATH:/root/google-cloud-sdk/bin
 
-pg_dumpall | gzip | gsutil cp - gs://$BUCKET/$PERFIX$(date +"%Y%m%d%H%M%S")$SUFFIX.sql.gz
+pg_dumpall | gzip | gsutil cp - gs://$BUCKET/$PREFIX$(date +"%Y%m%d%H%M%S")$SUFFIX.sql.gz
