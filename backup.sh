9c858a06ad3ff465291b438c39ce1bfcfbf60db1
Thanatat Tamtan <acoshift@gmail.com>
2019-11-20 09:29:20 +0700

    use pg_dump when specify PGDATABASE env

--- backup.sh
+++ backup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 export PATH=$PATH:/root/google-cloud-sdk/bin
 
-pg_dumpall | gzip | gsutil cp - gs://$BUCKET/$PREFIX$(date +"%Y%m%d%H%M%S")$SUFFIX.sql.gz
+if [[ -z "${PGDATABASE}" ]]; then
+  pg_dumpall | gzip | gsutil cp - gs://$BUCKET/$PREFIX$(date +"%Y%m%d%H%M%S")$SUFFIX.sql.gz
+else
+  pg_dump | gzip | gsutil cp - gs://$BUCKET/$PREFIX$(date +"%Y%m%d%H%M%S")$SUFFIX.sql.gz
+fi
