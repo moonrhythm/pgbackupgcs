3a9cd40f2fc538fc4f7e40b156d1e11d9e7cb8cd
Thanatat Tamtan <acoshift@gmail.com>
2019-11-29 19:33:48 +0700

    add GOOGLE_APPLICATION_CREDENTIALS_JSON env support

--- backup.sh
+++ backup.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 export PATH=$PATH:/root/google-cloud-sdk/bin
 
+if [[ ! -z "${GOOGLE_APPLICATION_CREDENTIALS_JSON}"]]; then
+  echo $GOOGLE_APPLICATION_CREDENTIALS_JSON > /root/google-credential.json
+  export GOOGLE_APPLICATION_CREDENTIALS=/root/google-credential.json
+fi
+
 if [[ -z "${PGDATABASE}" ]]; then
   pg_dumpall | gzip | gsutil cp - gs://$BUCKET/$PREFIX$(date +"%Y%m%d%H%M%S")$SUFFIX.sql.gz
 else
